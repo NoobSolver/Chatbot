02dcb76dc4a0417e13775133a67b32eb471c0156
Gaurav Kumar Mathur <mathuranant123@gmail.com>
2020-03-21 20:00:40 +0530

    Update destination

--- chatbot_train.py
+++ chatbot_train.py
@@ -7,7 +7,7 @@ def setup():
     chatbot = ChatBot('Bot',
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     trainer='chatterbot.trainers.ListTrainer')
-    for file in os.listdir('C:/Users/Prathmesh/Desktop/Python/Chatbot_Project-master/data/'):
+    for file in os.listdir('C:/Users/Gaurav/Desktop/Python/Chatbot_Project-master/data/'):
         convData = open(r'C:/Users/Prathmesh/Desktop/Python/Chatbot_Project-master/data/' + file,encoding='latin-1').readlines()
         chatbot.set_trainer(ListTrainer)
         chatbot.train(convData)
